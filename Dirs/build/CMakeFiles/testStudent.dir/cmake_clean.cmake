4dd1b6514e48f7a6454878e1f5e61ad0d70937ce
xiaowu-d3 <taitran411789@gmail.com>
2024-09-26 11:08:23 +0700

    cmake lib

---
+++ Dirs/build/CMakeFiles/testStudent.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testStudent.dir/src/Student.cpp.o"
+  "CMakeFiles/testStudent.dir/src/Student.cpp.o.d"
+  "testStudent"
+  "testStudent.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testStudent.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
